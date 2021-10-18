73c4c62ec3dd3a149f6de7d4ac641e2db2f91929
RoushanFW196 <roushankumar6338@gmail.com>
2021-10-18 16:28:18 +0530

    style adding

--- search.js
+++ search.js
@@ -1,20 +1,23 @@
 
 
 
- var newsquery=document.getElementById("newssearch").value;
+   //var newsquery=document.getElementById("newssearch").value;
 
 
   async   function searchnews(newsquery){
   
-   // var newsquery=document.getElementById("newssearch").value;
+    var newsquery=document.getElementById("newssearch").value;
     
 
-    let res= await fetch(` http://newsapi.org/v2/everything?q=${newsquery}&sortBy=popularity&apiKey=4c16ccefc2524a2dad09158632f7861d`)
+    let res= await fetch(`http://newsapi.org/v2/everything?q=${newsquery}&apiKey=4c16ccefc2524a2dad09158632f7861d `)
+
+
+      //  https://newsapi.org/v2/everything?q=bitcoin&apiKey=4c16ccefc2524a2dad09158632f7861d
    
 let data= await res.json();
-  console.log(data)
+  console.log(data.articles)
 
-
+    appendnews(data.articles);
 
 }
 
@@ -25,11 +28,34 @@ let data= await res.json();
    function appendnews(newsarr){
 
    
-    newsarr
+    newsarr.forEach(element => {
+        
+        let  newsdiv= document.createElement("div");
+
+       let p1=document.createElement("h3");
+       p1.textContent=element.title;
+
+       let image=document.createElement("img");
+       image.src=element.urlToImage;
+       
+       let p2=document.createElement("p");
+       p2.textContent=element.description;
+
+       let p3=document.createElement("p");
+       p3.textContent=element.content;
+
+
+       newsdiv.append(p1,image,p2,p3)
 
 
 
 
+
+  document.getElementById("searchedcontainer").append(newsdiv)
+
+
+
+    });
 
 
    }
